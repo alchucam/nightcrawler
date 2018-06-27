1154b2c30575551a5fc8fd3a25c9446346e6e554
alchucam <alchucam@gmail.com>
2018-06-26 20:21:08 -0500

    initial commit

--- pythonProjects/nightcrawler/nightcrawler/apps/services/admin.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-
-from django.contrib import admin
-from nightcrawler.apps.services.models import *
-
-# Register your models here.
-admin.site.register(newsData)
-admin.site.register(collectedData)
