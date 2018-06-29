b25744d32f3a96fde6b7842b998c2ab30e0fdc25
alchucam <alchucam@gmail.com>
2018-06-29 18:34:13 -0500

    fixing periodic tasks

--- nightcrawler/apps/services/management/commands/clock.py
+++ nightcrawler/apps/services/management/commands/clock.py
@@ -1,21 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
 from apscheduler.schedulers.blocking import BlockingScheduler
 from nightcrawler.apps.services.models import *
 
 #for custom periodic tasks
 #delete old data that is more than 8 days.
 
-sched = BlockingScheduler()
+class Command(BaseCommand):
+    help = 'delete old data that is more than 7 days'
 
-@sched.scheduled_job('interval', hours=1)
-def timed_job_testing():
-    print('This job is run every thirty minutes.')
-
-#@sched.scheduled_job('interval', days=8)
-@sched.scheduled_job('interval', minutes=10)
-def delete_old_data():
-    print("deleting old data...")
-    newsData.delete_old()
-    collectedData.delete_old()
-    print("delete finished!")
-
-sched.start()
+    def handle(self, *args, **options):
+        print("calling custom command")
+        sched = BlockingScheduler()
+        @sched.scheduled_job('interval', minutes=10)
+        def delete_old_data():
+            print("Start custom periodic tasks")
+            newsData.delete_old()
+            collectedData.delete_old()
+            print("Finish custom periodic tasks")
+        sched.start()
+        print("finish calling custom command")
